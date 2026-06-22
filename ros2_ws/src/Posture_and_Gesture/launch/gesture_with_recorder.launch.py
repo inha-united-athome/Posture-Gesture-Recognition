1bae311041c6f6915decd8b3d9d882760ccd1113
ParkGunwoo <adgjl06@naver.com>
2026-06-22 19:11:01 +0900

    bug fixed

--- ros2_ws/src/Posture_and_Gesture/launch/gesture_with_recorder.launch.py
+++ ros2_ws/src/Posture_and_Gesture/launch/gesture_with_recorder.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
@@ -59,7 +60,7 @@ def generate_launch_description():
                 "image_transport": image_transport,
                 "service_name": "set_recording",
                 "output_root_dir": recorder_output_dir,
-                "video_fps": recorder_fps,
+                "video_fps": ParameterValue(recorder_fps, value_type=float),
                 "video_fourcc": "mp4v",
             }],
         ),
